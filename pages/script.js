c8f9e521b46a0e8e658fae75a83344289d5ce771
Tamino Hartmann <tamino.hartmann@gmail.com>
2015-12-03 13:07:39 +0100

    more functionality

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,18 @@
+function prepare() {
+    // define what to do for all dragable elements
+    $("[draggable=true]").each(function(index, element){
+        // on drag start
+        $(element).on("dragstart", function(){
+            $("#output").text("Drag started with <"+ $(element).text()+">")
+        })
+        // on drag end
+        $(element).on("dragend", function(){
+            $("#output").text("Drag ended with <"+$(element).text()+">")
+            // append data to text field?
+        })
+    })
+}
+
 function test() {
-    alert("Hello World, JS version!")
+    $("#output").text("Hello JS and JQuery World!")
 }
